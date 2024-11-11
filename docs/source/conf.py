172dd8c3071bfaf34d6dd5b44c3184da23af9ad9
cxj <fusion@sjtu.edu.cn>
2024-11-11 15:04:25 +0800

    init Structure of TrackingToolBox docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'TrackingToolBox'
+copyright = '2024, SJTU fusion'
+author = 'Shawn Chen'
 
 release = '0.1'
 version = '0.1.0'
